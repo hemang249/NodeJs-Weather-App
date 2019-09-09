65aa1a0dd3c462f4d7548241c5ece1a126418a7d
Hemang Bhogayata <hemang249@gmail.com>
2019-09-09 22:26:52 +0530

    Added Location Based Coordinate Finding Using MapBox API

--- app.js
+++ app.js
@@ -1,11 +1,34 @@
 const request = require('request')
 
-const url = 'https://api.darksky.net/forecast/0453b711f5fba6baf467099cabe51601/37.8267,-122.4233?units=si'
-request({url : url , json : true} , (error , response)=>{
-    const data = response.body
-    const temp = data.currently.temperature
-    const rainPercent = data.currently.precipProbability * 100
-    
+const weatherURL= 'https://api.darksky.net/forecast/0453b711f5fba6baf467099cabe51601/37.8267,-122.4233?units=si'
+const location = 'Vadodara'
+const geolocationURL = 'https://api.mapbox.com/geocoding/v5/mapbox.places/'+ location +'.json?access_token=pk.eyJ1IjoiaGVtYW5nMjQ5IiwiYSI6ImNrMGNsdmVqaTAzMHgzaG8xZWhudGhrancifQ.Dl0u8ML51OUxluA_ys41hQ'
 
-    console.log(`${data.daily.data[0].summary} Currently it is ${temp} Celsius with a ${rainPercent} % Chance of Rain`)
+request({url : geolocationURL , json : true} , (error , response)=>{
+    
+    if(error){ 
+        console.log('Connection Interrupted! Make Sure you have a stable internet Connection')
+    } else if(response.body.message){
+        console.log('Error ! Location Not Found')
+    } else {
+        const data  = response.body 
+        const placeName = data.features[0].place_name
+        const latitude = data.features[0].geometry.coordinates[0]
+        const longitude = data.features[0].geometry.coordinates[1]
+        console.log(`${placeName} located at coordinates ${latitude} , ${longitude}`)
+    }
+    
+})
+
+request({url : weatherURL , json : true} , (error , response)=>{
+    if(error){
+        console.log('Connection Interrupted! Make Sure you have a stable internet Connection')
+    } else if(response.body.error){
+        console.log('Error ! Location Not Found !')
+    } else {
+        const data = response.body
+        const temp = data.currently.temperature
+        const rainPercent = data.currently.precipProbability * 100
+        console.log(`${data.daily.data[0].summary} Currently it is ${temp} Celsius with a ${rainPercent} % Chance of Rain`)
+    }   
 })
