3d1e44fd542160fa23f320385ecce555c370e3bd
Hemang Bhogayata <hemang249@gmail.com>
2019-09-10 22:01:25 +0530

    Added Dynamic Webpage Serving

--- src/web-server.js
+++ src/web-server.js
@@ -1,14 +1,29 @@
 const express = require('express')
 const path = require('path')
+const hbs = require('hbs')
 
+// Initialize Express JS
 const app = express()
-const publicDir = path.join(__dirname , '../public')
 
+// Paths For Express JS
+const publicDir = path.join(__dirname , '../public')
+const viewsDir = path.join(__dirname ,'../Templates/views')
+const partialsDir = path.join(__dirname , '../Templates/partials')
+
+// Setup HandleBars
 app.set('view engine', 'hbs')
+app.set('views', viewsDir)
+hbs.registerPartials(partialsDir)
+
+// Serving Public Dir
 app.use(express.static(publicDir))
 
 app.get('' , (req , res)=>{
     res.render('index')
+})
+
+app.get('*' , (req , res)=>{
+    res.send('Error 404 ! Page Not Found  ')
 })
 
 app.listen(3000 , ()=>{
