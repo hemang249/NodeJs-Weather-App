049ca8395087bac1a0df76d6ea2ad24692d50276
Hemang Bhogayata <hemang249@gmail.com>
2019-09-13 12:37:23 +0530

    Weather Rendering Setup

--- public/js/app.js
+++ public/js/app.js
@@ -1,15 +1,37 @@
 let locationInput = document.querySelector('#search-location')
 let address = ''
 
+const renderWeather = function(summary , temp , rainPercent){
+    
+    const weatherDiv = document.querySelector('.weather')
+    weatherDiv.innerHTML = ''
+    
+    const summaryEl = document.createElement('h4')
+    summaryEl.textContent = 'Weather Summary : ' + summary
+    
+    const tempEl = document.createElement('h4')
+    tempEl.textContent = 'Temperature : ' + temp + ' Celcius'
+
+    const rainEl = document.createElement('h4')
+    rainEl.textContent = 'Rain Chance : ' + rainPercent + '%'
+
+    weatherDiv.appendChild(summaryEl)
+    weatherDiv.appendChild(tempEl)
+    weatherDiv.appendChild(rainEl)    
+
+}
+
 locationInput.addEventListener('change',(e)=>{
     address = locationInput.value
     console.log(address)
     
     fetch('/weather?address='+address).then((response)=>{
         response.json().then((data)=>{
-            console.log(data)
+            renderWeather(data.summary   , data.temperature , data.rainPercent)
         })
     })
+
+    
 })
 
 
